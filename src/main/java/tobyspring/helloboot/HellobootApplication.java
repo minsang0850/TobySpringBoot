5d74edebc95ab58de9cd23c80d5d0a919a9a3e24
박민상 <minsang0850@sk.com>
2024-01-06 21:35:10 +0900

    서블릿 등록 및 요청 처리

---
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -0,0 +1,40 @@
+package tobyspring.helloboot;
+
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
+
+public class HellobootApplication {
+
+    public static void main(String[] args) {
+        //톰캣 서블릿 웹서버를 만들어주는 클래스
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    String name = req.getParameter("name");
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println("Hello " + name);
+                }
+            }).addMapping("/hello");
+        });
+        webServer.start();
+    }
+
+}
