eab42f88dcd84506b6dfda2cbc0530f00cca1d38
박민상 <minsang0850@sk.com>
2024-01-08 23:07:35 +0900

    애노테이션 매핑 정보 사용

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,15 @@
 package tobyspring.helloboot;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Objects;
 
+@Controller //부트 3.0부터는 RequestMapping만으로 핸들러에 등록 x Controller 어노테이션이 필요
+@RequestMapping
 public class HelloController {
     private final HelloService helloService;
 
@@ -9,6 +17,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping("/hello")
+    @ResponseBody //view가 아닌 String 자체를 return
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
