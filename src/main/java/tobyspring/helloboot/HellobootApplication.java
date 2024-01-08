740efc58f2bd31999dfbbc67bdf16ed1943a4cae
박민상 <minsang0850@sk.com>
2024-01-08 22:19:17 +0900

    DI 적용

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -20,12 +21,42 @@ import java.io.IOException;
 
 public class HellobootApplication {
 
+//    public static void main(String[] args) {
+//        //톰캣 서블릿 웹서버를 만들어주는 클래스
+//        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+//
+//        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+//            HelloController helloController = new HelloController();
+//            servletContext.addServlet("frontcontroller", new HttpServlet() {
+//                @Override
+//                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//                    //인증, 보안, 다국어, 공통 기능
+//                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+//                        String name = req.getParameter("name");
+//
+//                        String ret = helloController.hello(name);
+//
+//                        resp.setStatus(HttpStatus.OK.value());
+//                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+//                        resp.getWriter().println(ret);
+//                    } else {
+//                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+//                    }
+//                }
+//            }).addMapping("/*");
+//        });
+//        webServer.start();
+//    }
+
     public static void main(String[] args) {
         //톰캣 서블릿 웹서버를 만들어주는 클래스
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.refresh();
 
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            HelloController helloController = new HelloController();
             servletContext.addServlet("frontcontroller", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -33,10 +64,10 @@ public class HellobootApplication {
                     if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                         String name = req.getParameter("name");
 
+                        HelloController helloController =  applicationContext.getBean(HelloController.class);
                         String ret = helloController.hello(name);
 
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
                         resp.getWriter().println(ret);
                     } else {
                         resp.setStatus(HttpStatus.NOT_FOUND.value());
