49ccfdc65a929c91e4e793c6ff79e76260e37bcf
mingsang <minsang0850@naver.com>
2024-01-23 00:49:03 +0900

    DI를 이용한 Decorator, Proxy 패턴

--- src/test/java/tobyspring/helloboot/HelloServiceTest.java
+++ src/test/java/tobyspring/helloboot/HelloServiceTest.java
@@ -15,4 +15,11 @@ class HelloServiceTest {
 
         Assertions.assertThat(ret).isEqualTo("Hello Test");
     }
+
+    @Test
+    void helloDecorator() {
+        HelloDecorator decorator = new HelloDecorator(name -> name);
+        String ret = decorator.sayHello("Test");
+        Assertions.assertThat(ret).isEqualTo("*Test*");
+    }
 }
